b29b69aac68226081a5ee93018a8789f8fd803b2
Aakash Prajapati <aakashprajapati1001@gmail.com>
2025-06-22 14:37:17 +0530

    add certificates

--- src/component/footer/index.jsx
+++ src/component/footer/index.jsx
@@ -2,7 +2,9 @@ import PropTypes from "prop-types";
 import { colors, transition } from "../../constants";
 
 const Footer = ({ isDarkMode }) => {
-  const { background, text, accent,link } = isDarkMode ? colors.dark : colors.light;
+  const { background, text, accent, link } = isDarkMode
+    ? colors.dark
+    : colors.light;
 
   return (
     <footer className={`pt-14 pb-6 ${background} ${transition.duration}`}>
@@ -11,13 +13,16 @@ const Footer = ({ isDarkMode }) => {
           <div className={`${text}`}>
             &copy; {new Date().getFullYear()} All rights reserved.
             <br />
-            <div className={`${accent} flex justify-center items-center`}>Created by:              <a
-              href={'https://www.linkedin.com/in/aakash-prajapati-020791302'}
-              className={`${link} hover:underline`}
-              target="_blank"
-            >
-             &nbsp;Aakash Prajapati
-            </a></div>
+            <div className={`${accent} flex justify-center items-center`}>
+              Created by:{" "}
+              <a
+                href={"https://www.linkedin.com/in/aakashprajapati1001/"}
+                className={`${link} hover:underline`}
+                target="_blank"
+              >
+                &nbsp;Aakash Prajapati
+              </a>
+            </div>
           </div>
         </div>
       </div>
